c9fb08d2e482150c4c10bdfc9082a88b771657cf
kikusujun <kikusujun@gmail,com>
2024-04-15 18:11:19 +0900

    handle.ts

---
+++ N1赤宝書/handle.ts
@@ -0,0 +1,34 @@
+import { readFileSync } from "fs";
+import * as path from "path";
+
+const folder_path: string = "./わ";
+const file_name: string = "わ.md";
+
+const filePath: string = path.join(folder_path, file_name);
+
+console.log(filePath);
+
+const data: string = readFileSync(filePath, { encoding: "utf8" });
+console.log(data);
+
+const tangoArr: string[] = data.split("\n");
+const reg: RegExp = /^\S/;
+tangoArr
+  .filter((_v, i) => i < 100)
+  .forEach((line) => {
+    console.log(line);
+    console.log(reg.test(line));
+    // wrap ruby tag
+    if (reg.test(line)) {
+      const reg0 = /([\p{sc=Han}々]+)([\p{sc=Hiragana}]+)/ug
+      const reg1 = /([\p{sc=Katakana}ー]+)([a-zA-Z ・]+)/ug
+      const reg2 = /　(\p{sc=Hiragana}+)/ug
+      const reg3 = /\p{sc=Hiragana}/ug
+      const res: string = 
+        line.replace(reg0, "$1<rt>$2</rt>") // 
+        .replace(reg1, '$1<rt>$2</rt>')
+        .replace(reg2, '$1<rt></rt>')
+        ; // 
+      console.log(res);
+    }
+  });
